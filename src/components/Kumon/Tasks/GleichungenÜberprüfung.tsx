972f7d9bcae32e6ebfa4b364f8cd617aa862f9f9
lancey <lanceyanosike10@gmail.com>
2023-11-29 22:53:15 +0100

    überprüfer

--- src/components/Kumon/Tasks/GleichungenÜberprüfung.tsx
+++ src/components/Kumon/Tasks/GleichungenÜberprüfung.tsx
@@ -1,68 +1,7 @@
 
 
- //# function checkAndReduceEquation(equation: string) {
-
-   //# function gcd(a: number, b: number) {
-  //#  while (b !== 0) {
-        //#    const temp = b;
-        //#    b = a % b;
-         //#   a = temp;
-     //#   }
-      //#  return a;
-      //#  }
-
- //# const parts =
-//# equation.split('=').map(part =>
-//#part.trim());
-
-   //# if (parts.length < 2) {
-       //# return "Ungültige Gleichung.";
-   //# }
-
-   //# for (let i = 0; i < parts.length - 1; i++) {
-  //#  const [numerator1, denominator1] =
-//#parts[i].split('/').map(part =>
-//#parseInt(part, 10));
-  //#  const [numerator2, denominator2]  =
-//#parts[i + 1].split('/').map(part =>
-//#parseInt(part, 10));
-
-    //# if (numerator1 * denominator2 !==
-       //# numerator2 * denominator2) {
-       //#     return "Die Gleichung ist nicht korrekt.";
-      //#  }  
-
-     //#   if (denominator1 ! == 0 && denominator2 !== 0) {
-//#const commonDivisor1 = gcd(numerator1,denominator1);
-//#const commonDivisor2 = gcd(numerator2,denominator2)
-
- //#const reducedNumerator1 = numerator1 /commonDivisor1;
- //#const reducedDenominator1 = denominator1/ commonDivisor1;
- //#const reducedNumerator2 = numerator2 / commonDivisor2;
- //#const  reducedDenominator2 = denominator2 / commonDivisor2;
-        
-
-      //#  parts[i] = `${reducedNumerator1}/${reducedDenominator1}`;
-      //#  parts[i + 1] = `${reducedNumerator2}/${reducedDenominator2}`;
-         //#   }
-  //#  } 
-
-   //# return `${equation} ist korekkt und gekürzt.`;
-//#}
-
-
-
- //# let exampleEquation = "36/12 = 18/6 = 9/3";
- //#let result = 
-//#checkAndReduceEquation(exampleEquation);
-
-//#console.log(result);  
-
  
-// Beispielaufruf
-let exampleEquation = "48/12 = 18/4 = 48/12";
-let result = checkAndReduceEquation(exampleEquation);
-console.log(result);
+
 
 function checkAndReduceEquation(equation: string) { //string entfernen
   // Hilfsfunktion für den größten gemeinsamen Teiler (GCD)
@@ -138,3 +77,7 @@ function checkAndReduceEquation(equation: string) { //string entfernen
   return `${equation} ist korrekt und gekürzt.`;
 }
 
+// Beispielaufruf
+let exampleEquation = "48/12 = 18/4 = 48/12";
+let result = checkAndReduceEquation(exampleEquation);
+console.log(result);
