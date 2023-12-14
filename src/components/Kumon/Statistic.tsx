72aba10bf3b36a92e4d51a6cbe33ccc414822007
lancey <lanceyanosike10@gmail.com>
2023-12-14 22:57:58 +0100

    counter mit Reset

--- src/components/Kumon/Statistic.tsx
+++ src/components/Kumon/Statistic.tsx
@@ -2,34 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, Switch, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
 
 const Statistic= ({route}: any) => {
     
     
 
 
-const CounterComponent: React.FC = () => {
+
   const[counter, setCounter] =
   useState(0)
 
   const handlePress = () => {
     setCounter(counter + 1);
   };
+
+const handleReset =() => {
+  setCounter(0);
+}
   return(
     <View style={styles.container}>
-      <TouchableOpacity
-onPress={handlePress} style={styles.button}
->
+      <Text
+onPress={handlePress} style={styles.button}>
   <Text style={styles.buttonText}
-  >CDrück mich</Text>
-  </TouchableOpacity>
+  >Drück mich</Text>
+  </Text>
+  <Text 
+  onPress={handleReset} style={styles.button}
+  >
+    <Text style={styles.buttonText}
+  >Reset</Text>
+  </Text>
+  
   <Text style={styles.counterText}
   >Counter: {counter}</Text>
   </View>
   );
-}};
+};
 
 
 const styles = StyleSheet.create({
@@ -40,16 +51,16 @@ const styles = StyleSheet.create({
   },
 button: {
   padding: 10,
-  backgroundColor: 'black',
+  backgroundColor: 'blue',
   borderRadius: 5,
   marginBottom: 10,
 },
 buttonText: {
   color: 'black',
-  fontSize: 16,
+  fontSize: 20,
 },
 counterText: {
-  fontSize: 18,
+  fontSize: 20,
 },
 });
 
