fb3286c36f119dc03a7683339fae0edeb5cefff4
lancey <lanceyanosike10@gmail.com>
2023-12-14 17:03:03 +0100

    c

--- src/components/Kumon/Statistic.tsx
+++ src/components/Kumon/Statistic.tsx
@@ -11,22 +11,8 @@ const Statistic= ({route}: any) => {
   }
 
 export default Statistic;
-let counter: number = 0
-const counterButton : HTMLButtonElement |
-null =
-document.getElementById("counterButton") as 
-HTMLButtonElement;
-const counterElement :  HTMLElement | null =
-document.getElementById("counter")
 
-if (counterButton && counterElement) {
-  counterButton.addEventListener('click',
-()  => {
-     counter++;
-     if (counterElement) {
-      counterElement.textContent =
-      `Counter: ${counter}`;
-     }
-     });
-    }
+
+
+
 
