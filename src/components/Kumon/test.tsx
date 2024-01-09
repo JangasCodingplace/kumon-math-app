00d1d7829451b46f8cec1197f173e649d7244e0f
lancey <lanceyanosike10@gmail.com>
2024-01-09 23:38:31 +0100

    12

--- src/components/Kumon/test.tsx
+++ src/components/Kumon/test.tsx
@@ -10,89 +10,55 @@ interface MathProblem {
     answer: number;
 }
 const mathProblems: MathProblem[] = [
-     {question: '2 + 2', answer: 4},
+     {question: '2 + 2', answer:4 },
      {question: '44 - 2', answer: 42},
      {question: ' 1 + 1', answer: 2},
      {question: '25 + 3', answer: 28},
      {question: '3 - 2', answer: 1},
+    
 ];
 
-const Statistic = () => {
-    const [userAnswers, setUserAnswers] =
-    useState<number[]>(new
-        Array(mathProblems.length).fill(0));
-        const [results, setResults] =
-        useState<string[]>(new
-            Array(mathProblems.length).fill(''));
-
-        const checkAnswers =  () => {
-            const newResults: string[] = [];
-            for (let i = 0; i<
-                mathProblems.length; i++) {
-                    if (userAnswers[i] ===
-                    mathProblems[i].answer) {
-                        newResults.push('Richtig!');
-                        
-                    } else {
-                        newResults.push(`Falsch. Die richtige Antwort ist ${mathProblems[i].answer}.`);
-
-                    }
-                }
-                setResults(newResults)
-                Keyboard.dismiss();
-        };
-
-        return (
-            <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ?
-        'padding': 'height'}
-        style={styles.container}
-        >
-            <View style={styles.inner}>
-            {mathProblems.map((problem, index) =>
+const Statistic = () => (
+    
+   <View style={styles.container} >
+    <View style={styles.inner}>
+        
+       {mathProblems.map(({question,answer}, index) =>
+              
 (
     <View key={index}
     style={styles.problemContainer}>
-        <Text>{problem.question}</Text>
-        <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        onChangeText={(text) => {
-            const newAnswers = 
-            [... userAnswers];
-            newAnswers[index] = 
-            parseInt(text, 10) || 0;
-            setUserAnswers(newAnswers);
-        }}
-        />
-        <Text>{results[index]}</Text>
+        <Text>{`${question} =  ${answer}` }</Text>
+        <Text style={{color: answer === eval(question) ? 'green' : 'red'}}>
+        {answer ===  eval(question) ? 'True' : 'False'}
+       </Text>
       </View>
 ))}
-<Button title="Antworten überprüfen"
-onPress={checkAnswers} />
 </View>
-</KeyboardAvoidingView>
-);
-    };
+</View>
+        );
 
     const styles = StyleSheet.create({
         container: {
-            flex:1
+            flex:1,
         },
-           inner: {
+             inner: {
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
             padding: 20
         },
         problemContainer: {
-            marginBottom: 10,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+             marginBottom: 20,
         },
-        input: {
-            borderWidth: 1,
-            borderColor: '#ccc',
-            padding: 10,
-            marginTop: 5,
-        },
+answerContainer: {
+marginLeft: 30,
+},
+answer: {
+    fontSize: 18,
+},
+       
     });
     export default Statistic ;
