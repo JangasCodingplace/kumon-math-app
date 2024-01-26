5d170d2eaad98af39e384c3b8baacc1f90f44a73
Konradpina <ksentek@gmx.de>
2024-01-26 20:23:21 +0100

    Change4

--- src/components/Kumon/Profiles.ts
+++ src/components/Kumon/Profiles.ts
@@ -4,8 +4,18 @@ import SimpleSubtraction from "./Tasks/SimpleSubtraction";
 import RootGrade2 from "./Tasks/RootGrade2";
 import Addition10 from "./Tasks/Addition10";
 import addition100 from "./Tasks/addition100";
-import Multiplication100 from "./Tasks/Muliplikation10";
+import addition1000 from "./Tasks/addition1000";
+
+import Multiplication10 from "./Tasks/Muliplikation10";
+import Multiplication100 from "./Tasks/Multiplication100";
+
+import substraction100 from "./Tasks/subtraction100";
+import subtraction1000 from "./Tasks/subtraction1000";
+
 import division100 from "./Tasks/division100";
+import division10 from "./Tasks/division10";
+import PunktVorStrichPlus from "./Tasks/PunktVorStrichPlus";
+
 
 interface Profile {
   name: string
@@ -16,18 +26,21 @@ interface Profile {
 const profiles: Profile[] = [
   {
     name: "Addition",
-    taskTypes: [Addition10, addition100]
+    taskTypes: [ Addition10,addition100,addition1000]
   },
   {
     name: "Subtrahieren",
-    taskTypes: [RootGrade2]
+    taskTypes: [substraction100,subtraction1000]
   },
   {
     name: "Multiplikation",
-    taskTypes: [Multiplication100]
+    taskTypes: [Multiplication10,Multiplication100]
   },{
     name: "Division",
-    taskTypes: [division100,]
+    taskTypes: [division10,division100,]
+  },{
+    name: "PunktVorStrichPlus",
+    taskTypes: [PunktVorStrichPlus,]
   }
 ]
 
