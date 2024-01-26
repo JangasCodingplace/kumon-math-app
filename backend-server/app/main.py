039b3e58b91e0259185c1486aa3b6ee95c6884b3
GithubG1234 <gvandyken@t-online.de>
2024-01-26 19:46:01 +0100

    create pupil accounts

--- backend-server/app/main.py
+++ backend-server/app/main.py
@@ -1,10 +1,44 @@
+import random
+import string
 from typing import Union
-
 from fastapi import FastAPI
+from dataclasses import dataclass
+
+
+@dataclass
+class GeneratePupilPayload:
+    n: int
+    teacher_id: str
+    root_lvl: int
+
 
 app = FastAPI()
+
+
+def generate_random_string(length=5):
+    """Generiere einen zufälligen String mit fester Länge."""
+    characters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+@app.post("/create-pupil-accounts")
+def create_pupil_accounts(payload: GeneratePupilPayload):
+    """Erstelle n Benutzerkonten ohne MongoDB."""
+    user_accounts = []
+
+    for _ in range(payload.n):
+        user_id = generate_random_string()
+        user_data = {
+            'id': user_id,
+            'teacher_id': payload.teacher_id,
+            'root_lvl': payload.root_lvl
+        }
+        user_accounts.append(user_data)
+
+    return {"data":user_accounts}
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
