e2bfcabb24923f3cc7fd65e4c47c19d1f905ddaa
nisrinecvc <123650875+nisrinecvc@users.noreply.github.com>
2024-01-26 17:49:41 +0100

    addition1000

---
+++ src/components/Kumon/Tasks/division100.tsx
@@ -0,0 +1,19 @@
+import { version } from "react";
+import { division100 } from "../GenericTask";
+
+
+class division100 extends GeneratedSimpleCalcTask {
+  public static taskId = "simple-addition"
+  public static generate(): division100 {
+    const firstNumber = Math.floor(Math.random() * 100);
+    const secondNumber = Math.floor(Math.random() * 100);
+    const simpleTask = `${firstNumber} : ${secondNumber}`;
+    const latexTask = `${firstNumber} : ${secondNumber}`;
+    const simpleSolution = `${firstNumber + secondNumber}`;
+    const latexSolution = `${firstNumber + secondNumber}`;
+    const version="2"
+    return new division100(simpleTask, latexTask, simpleSolution, latexSolution,version);
+  }
+}
+
+export default division100;
