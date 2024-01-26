3a72c70f010cea65944c589f1e6d1d5a99528bd0
Alen776 <143722229+Alen776@users.noreply.github.com>
2024-01-26 18:08:10 +0100

    mgh

--- src/components/Kumon/Tasks/SimpleSubtraction.tsx
+++ src/components/Kumon/Tasks/SimpleSubtraction.tsx
@@ -2,10 +2,10 @@ import { version } from "react";
 import { GeneratedSimpleCalcTask } from "../GenericTask";
 
 
-class SimpleSubtraction extends GeneratedSimpleCalcTask {
-  public static taskId = "simple-substraction"
+class substraction10 extends GeneratedSimpleCalcTask {
+  public static taskId = "substraction10"
 
-  public static generate(): SimpleSubtraction {
+  public static generate(): substraction10 {
     const firstNumber = Math.floor(Math.random() * 10);
     const secondNumber = Math.floor(Math.random() * 10);
     const simpleTask = `${firstNumber} - ${secondNumber}`;
@@ -13,8 +13,8 @@ class SimpleSubtraction extends GeneratedSimpleCalcTask {
     const simpleSolution = `${firstNumber - secondNumber}`;
     const latexSolution = `${firstNumber - secondNumber}`;
     const version="4"
-    return new SimpleSubtraction(simpleTask, latexTask, simpleSolution, latexSolution,version);
+    return new substraction10(simpleTask, latexTask, simpleSolution, latexSolution,version);
   }
 }
 
-export default SimpleSubtraction;
+export default substraction10;
