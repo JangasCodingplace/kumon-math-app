5a8fbdf80b5c2febbf08776c4331eefaaeef5399
Konradpina <ksentek@gmx.de>
2024-01-26 17:46:42 +0100

    Changes

--- src/components/Kumon/Profiles.ts
+++ src/components/Kumon/Profiles.ts
@@ -2,7 +2,9 @@ import { GeneratedSimpleCalcTask } from "./GenericTask";
 import SimpleAddition from "./Tasks/SimpleAddition";
 import SimpleSubtraction from "./Tasks/SimpleSubtraction";
 import RootGrade2 from "./Tasks/RootGrade2";
-
+import Addition10 from "./Tasks/Addition10";
+import addition100 from "./Tasks/addition100";
+import Multiplication100 from "./Tasks/Muliplikation10";
 
 interface Profile {
   name: string
@@ -12,15 +14,18 @@ interface Profile {
 
 const profiles: Profile[] = [
   {
-    name: "5-simple",
-    taskTypes: [SimpleAddition, SimpleSubtraction]
+    name: "Addition",
+    taskTypes: [Addition10, addition100]
   },
   {
-    name: "10-simple",
+    name: "Subtrahieren",
     taskTypes: [RootGrade2]
   },
   {
-    name: "11-ef",
+    name: "Multiplikation",
+    taskTypes: [Multiplication100]
+  },{
+    name: "Division",
     taskTypes: [RootGrade2]
   }
 ]
