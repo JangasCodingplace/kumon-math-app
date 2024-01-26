796d0eb1071365d6639621e758e6d04f9ea1d8d8
ahmet6262 <123650443+ahmet6262@users.noreply.github.com>
2024-01-26 18:11:09 +0100

    PunktVorStrichPlus

--- src/components/Kumon/Tasks/PunktVorStrichPlus.tsx
+++ src/components/Kumon/Tasks/PunktVorStrichPlus.tsx
@@ -2,9 +2,9 @@ import { version } from "react";
 import { GeneratedSimpleCalcTask } from "../GenericTask";
 
 
-class Multiplication10 extends GeneratedSimpleCalcTask {
-  public static taskId = "simple-Multiplication"
-  public static generate(): Multiplication10 {
+class PunktVorStrichPlus extends GeneratedSimpleCalcTask {
+  public static taskId = "PunktVorStrichPlus"
+  public static generate(): PunktVorStrichPlus {
     const firstNumber = Math.floor(Math.random() * 10);
     const secondNumber = Math.floor(Math.random() * 10);
     const thirdNumber = Math.floor(Math.random() + 100);
@@ -13,8 +13,8 @@ class Multiplication10 extends GeneratedSimpleCalcTask {
     const simpleSolution = `${firstNumber * secondNumber + thirdNumber}`;
     const latexSolution = `${firstNumber * secondNumber + thirdNumber}`;
     const version="2"
-    return new  Multiplication10(simpleTask, latexTask, simpleSolution, latexSolution,version);
+    return new  PunktVorStrichPlus(simpleTask, latexTask, simpleSolution, latexSolution,version);
   }
 }
 
-export default Multiplication10;
+export default PunktVorStrichPlus;
