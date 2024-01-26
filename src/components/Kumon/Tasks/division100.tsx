8555c5dfc7d6606a4ee417393cdad384304f9931
nisrinecvc <123650875+nisrinecvc@users.noreply.github.com>
2024-01-26 18:11:47 +0100

    nis

--- src/components/Kumon/Tasks/division100.tsx
+++ src/components/Kumon/Tasks/division100.tsx
@@ -3,14 +3,14 @@ import { GeneratedSimpleCalcTask } from "../GenericTask";
 
 
 class division100 extends GeneratedSimpleCalcTask {
-  public static taskId = "simple-addition"
+  public static taskId = "division100"
   public static generate(): division100 {
     const firstNumber = Math.floor(Math.random() * 100);
     const secondNumber = Math.floor(Math.random() * 100);
     const simpleTask = `${firstNumber} : ${secondNumber}`;
     const latexTask = `${firstNumber} : ${secondNumber}`;
-    const simpleSolution = `${firstNumber + secondNumber}`;
-    const latexSolution = `${firstNumber + secondNumber}`;
+    const simpleSolution = `${firstNumber / secondNumber}`;
+    const latexSolution = `${firstNumber / secondNumber}`;
     const version="2"
     return new division100(simpleTask, latexTask, simpleSolution, latexSolution,version);
   }
