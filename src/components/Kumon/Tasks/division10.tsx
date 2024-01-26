a1429afc00116d5e4233b363e4eea9b5e226f967
dijana <123250133+dijanacvc@users.noreply.github.com>
2024-01-26 18:15:42 +0100

    divison10

--- src/components/Kumon/Tasks/division10.tsx
+++ src/components/Kumon/Tasks/division10.tsx
@@ -3,14 +3,15 @@ import { GeneratedSimpleCalcTask } from "../GenericTask";
 
 
 class division10 extends GeneratedSimpleCalcTask {
-  public static taskId = "divison10"
+  public static taskId = "division10"
   public static generate(): division10 {
     const firstNumber = Math.floor(Math.random() * 10);
     const secondNumber = Math.floor(Math.random() * 10);
-    const simpleTask = `${firstNumber} : ${secondNumber}`;
-    const latexTask = `${firstNumber} : ${secondNumber}`;
-    const simpleSolution = `${firstNumber / secondNumber}`;
-    const latexSolution = `${firstNumber /secondNumber}`;
+    const thirdnumber = firstNumber * secondNumber;
+    const simpleTask = `${thirdnumberNumber} : ${firstNumber}`;
+    const latexTask = `${thirdNumber} : ${firstNumber}`;
+    const simpleSolution = `${secondNumber}`;
+    const latexSolution = `${secondNumber}`;
     const version="2"
     const result = firstNumber % secondNumber === 0 ? firstNumber / secondNumber : firstNumber;
     return new division10(simpleTask, latexTask, simpleSolution, latexSolution,version);
