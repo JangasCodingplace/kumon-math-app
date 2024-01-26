7f1614fa100de704dade9f414dc0c1feae2f0a71
Alen776 <143722229+Alen776@users.noreply.github.com>
2024-01-26 19:45:35 +0100

    Potenzenhoch2

--- src/components/Kumon/Tasks/PotenzenHoch2.tsx
+++ src/components/Kumon/Tasks/PotenzenHoch2.tsx
@@ -5,12 +5,12 @@ class Potenzen2 extends GeneratedSimpleCalcTask {
   public static taskId = "Potenzen2"
 
   public static generate(): Potenzen2{
-    let base = Math.floor(Math.random() * 10+1);
-    let exponent = Math.floor(Math.const() * 2)
-    let simpleTask = base + '^' + exponent;
-   let  latexTask = base + '^' + exponent;;
-   let  simpleSolution = base + '^' + exponent;
-    let latexSolution =base + '^' + exponent; 
+   const base = Math.floor(Math.random() * 10+1);
+   const exponent = Math.floor(Math.random() * 2)
+   const simpleTask = `${base} + '^' + ${exponent}`;
+  const  latexTask = `${base} + '^' + ${exponent}`;;
+  const  simpleSolution = `${base} + '^' + ${exponent}`;
+   const latexSolution =`${base} + '^' + ${exponent}`; 
     return new Potenzen2(simpleTask, latexTask, simpleSolution, latexSolution);
   }
 }
