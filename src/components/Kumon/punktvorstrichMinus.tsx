3356172f259c9795732033fd39753c915c86d10b
lanceyy05 <123355176+lanceyy05@users.noreply.github.com>
2024-01-27 09:57:33 +0100

    lancey

---
+++ src/components/Kumon/punktvorstrichMinus.tsx
@@ -0,0 +1,20 @@
+import { version } from "react";
+import { GeneratedSimpleCalcTask } from "../GenericTask";
+
+
+class Addition10 extends GeneratedSimpleCalcTask {
+  public static taskId = "simple-addition"
+  public static generate(): Addition10 {
+    const firstNumber = Math.floor(Math.random() * 10);
+    const secondNumber = Math.floor(Math.random() * 10);
+    const thridNumber = Math.floor(Math.random() * 10)
+    const simpleTask = `${firstNumber} * ${secondNumber}`;
+    const latexTask = `${firstNumber} * ${secondNumber}`;
+    const simpleSolution = `${firstNumber + secondNumber}`;
+    const latexSolution = `${firstNumber + secondNumber}`;
+    const version="2"
+    return new Addition10(simpleTask, latexTask, simpleSolution, latexSolution,version);
+  }
+}
+
+export default Addition10;
